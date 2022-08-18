0236432f7a19c12f8462ff93b0b8fe5cdca0eefc
covertimpala <77851547+covertimpala@users.noreply.github.com>
2022-08-18 22:56:56 +0200

    improvements

--- installer.py
+++ installer.py
@@ -1,1 +1,24 @@
+import subprocess
+import sys
+import colorama
+from colorama import Fore
 
+print("User Current Version:-", sys.version)
+print(Fore.RESET)
+
+
+name = 'SpeechRecognition'
+print("--==--")
+try:
+    subprocess.check_call([sys.executable, "-m", "pip", "show", name])
+    print(Fore.GREEN + "All requirements satisfied")
+
+except:
+    print("Packages need to be installed before Tess can be used")
+    print("installing packages")
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    except:
+        print(Fore.RED + "Installation failed")
+
+print(Fore.RESET)
